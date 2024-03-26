bd2591ea15fbc04f52250b98f71fe2e59f461364
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-03-26 09:09:20 +0100

    functionality: using  styled sytem to style the component

--- src/componets/navbar/Navbar.jsx
+++ src/componets/navbar/Navbar.jsx
@@ -1,1 +1,19 @@
 import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+const Navbar = () => {
+  const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+  });
+  const SocialBox = styled(Box)({
+    display: "flex",
+    gap: 10,
+  });
+  const MenuBox = styled(Box)({
+    display: "flex",
+    gap: 30,
+  });
+
+  const SearchBox = styled(Box)({
+    display: "flex",
+    gap: 5,
+  });
