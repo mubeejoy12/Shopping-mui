b467a81ce957ed57ee44a20830c6422d43e3bacd
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-03-26 09:07:03 +0100

    chore: rendering the  navbar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Button, Typography, Container } from "@mui/material";
+import AbcIcon from "@mui/icons-material/Abc";
+import { Box } from "@mui/system";
+import Navbar from "./componets/navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar/>
+    </>
+  );
 }
 
 export default App;
