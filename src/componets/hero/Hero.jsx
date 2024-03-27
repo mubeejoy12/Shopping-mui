72204eae2959be083c2dc9869c2a675501e596bb
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-03-28 00:42:19 +0100

    chore: rendering the hero page.

--- src/componets/hero/Hero.jsx
+++ src/componets/hero/Hero.jsx
@@ -1,3 +1,16 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+const Hero = () => {
+  return (
+    <Box>
+      <Typography align="center" variant="h3" sx={{ fontWeight: 900 }}>
+        {" "}
+        Fashions
+        <b style={{ color: "red" }}>Blog</b>
+      </Typography>
+    </Box>
+  );
+};
+
+export default Hero;
