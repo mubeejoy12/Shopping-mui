dbdaad0401fe7111d6a9e616c3a0e892c2f012b2
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-04-26 17:02:44 +0100

    adding new  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import Categories from "./componets/Categories/Categories";
+import Footer from "./componets/footer/Footer";
 import Hero from "./componets/hero/Hero";
 import Navbar from "./componets/navbar/Navbar";
+import Recents from "./componets/recents/Recents";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./componets/home/Home";
+import Details from "./componets/details/Details";
 
 function App() {
   return (
@@ -12,6 +18,8 @@ function App() {
           <Route path="/details" element={<Details />} />
         </Routes>
       </BrowserRouter>
+
+      <Footer />
     </>
   );
 }
